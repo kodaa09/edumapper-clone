6b16c8e7decde89f818b35200592fd0574a3b3a8
kodaa <je.elbaze@gmail.com>
2025-07-04 12:07:53 +0200

    add packages

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/image']
-})
+  modules: [
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@vee-validate/nuxt",
+  ],
+  css: ["~/assets/css/tailwind.css"],
+  runtimeConfig: {
+    public: {
+      apiBase: "http://localhost:3333/api",
+    },
+  },
+});
