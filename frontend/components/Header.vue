5ac8763283c36d12dbd3efecd0aa082762b6ab26
kodaa <je.elbaze@gmail.com>
2025-07-04 12:06:56 +0200

    do header and layouts

---
+++ frontend/components/Header.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { useAuthStore } from "~/store/auth";
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <div class="px-10 py-3 flex justify-between items-center">
+    <div class="flex items-center gap-4">
+      <h1 class="p-6 px-5">
+        <NuxtLink to="/">
+          <NuxtImg
+            class="object-contain w-[146px]"
+            src="/logo.png"
+            alt="logo"
+          />
+        </NuxtLink>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink class="font-medium" to="/"
+              >Mes chances d'admission</NuxtLink
+            >
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div v-if="!authStore.user">
+      <NuxtLink
+        to="/auth"
+        class="bg-primary text-black border rounded-full border-gray-300 px-6 py-3 text-lg hover:brightness-90 transition-colors duration-200"
+      >
+        Connexion
+      </NuxtLink>
+    </div>
+    <div class="flex items-center gap-4" v-else>
+      <p>
+        {{ authStore.user?.fullName }}
+      </p>
+      <button
+        class="bg-primary text-black border rounded-full border-gray-300 px-6 py-3 text-lg hover:brightness-90 transition-colors duration-200"
+        @click="authStore.logout"
+      >
+        Déconnexion
+      </button>
+    </div>
+  </div>
+</template>
